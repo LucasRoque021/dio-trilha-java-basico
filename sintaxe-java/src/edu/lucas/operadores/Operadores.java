9cc6beb80228ca952f68258ce6b22994cff2e4c2
LucasRoque021 <lucas.p.roque93@gmail.com>
2022-11-04 21:07:49 -0300

    atribuicao, atribuição, ops aritméticos e concat

--- sintaxe-java/src/edu/lucas/operadores/Operadores.java
+++ sintaxe-java/src/edu/lucas/operadores/Operadores.java
@@ -1,7 +1,42 @@
 package edu.lucas.operadores;
 
+import java.util.Date;
+
 public class Operadores {
   public static void main(String[] args) {
-    
+    // Atribuição -> =
+
+    String nome = "Lucas";
+    int idade = 29;
+    double peso = 86.3;
+    char sexo = 'M';
+    boolean doadorOrgao = false;
+    boolean doadorSangue = true;
+    Date dataNascimento = new Date();
+
+    // Operadores aritméticos -> +, -, *, /, %
+
+    int soma = 8 + 2;
+    int subtracao = 8 - 2;
+    int multiplicacao = 8 * 2;
+    double divisao = 8/2;
+
+    // + para concatenação
+    String nomeCompleto = "LINGUAGEM" + "JAVA";
+		
+    //qual o resultado das expressoes abaixo?
+    String concatenacao ="?"; //?
+
+    concatenacao = 1+1+1+"1";  //31
+
+    concatenacao = 1+"1"+1+1; //1111
+
+    concatenacao = 1+"1"+1+"1"; //1111
+
+    concatenacao = "1"+1+1+1; //1111
+
+    concatenacao = "1"+(1+1+1); //13
+
+
   }
 }
